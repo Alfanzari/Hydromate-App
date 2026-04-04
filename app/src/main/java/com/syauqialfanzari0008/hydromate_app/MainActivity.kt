6627dd0e34697ee677693469f6dab88ffe184eb4
Syauqi Al Fanzari <alfanzarisyauqi@gmail.com>
2026-04-03 23:48:06 -0700

    Memperbaiki data bug hilang

--- app/src/main/java/com/syauqialfanzari0008/hydromate_app/MainActivity.kt
+++ app/src/main/java/com/syauqialfanzari0008/hydromate_app/MainActivity.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -100,10 +101,11 @@ fun AppNavigation() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(onNavigateToResult: (String, Boolean) -> Unit) {
-    var weightInput by remember { mutableStateOf("") }
-    var isError by remember { mutableStateOf(false) }
-    var isActiveMode by remember { mutableStateOf(false) }
 
+    var weightInput by rememberSaveable { mutableStateOf("") }
+    var isActiveMode by rememberSaveable { mutableStateOf(false) }
+    
+    var isError by remember { mutableStateOf(false) }
     var showDialog by remember { mutableStateOf(false) }
 
     if (showDialog) {
